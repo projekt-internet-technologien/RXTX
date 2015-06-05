07e2f4322b77d196d6d9df813f8d15b0d00f8f3f
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-06-05 14:46:49 +0200

    RXTX works with Arduino UNO

---
+++ run.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+for port in `find /dev -name 'tty*'`
+do
+    PORTS="$PORTS:$port"
+done
+JAVA_OPT="-Djava.library.path=/usr/lib/jni -Dgnu.io.rxtx.SerialPorts=$PORTS"
+java -jar $JAVA_OPT target/RXTX-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/de/dennis_boldt/Main.java
@@ -0,0 +1,71 @@
+package de.dennis_boldt;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.LinkedList;
+
+public class Main {
+
+	void connect(String portName) throws Exception {
+		CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		if (portIdentifier.isCurrentlyOwned()) {
+			System.out.println("Error: Port is currently in use");
+		} else {
+			int timeout = 2000;
+			CommPort commPort = portIdentifier.open(this.getClass().getName(), timeout);
+
+			if (commPort instanceof SerialPort) {
+				SerialPort serialPort = (SerialPort) commPort;
+				serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+				InputStream in = serialPort.getInputStream();
+				OutputStream out = serialPort.getOutputStream();
+
+				(new Thread(new SerialReader(in))).start();
+				(new Thread(new SerialWriter(out))).start();
+
+			} else {
+				System.out.println("Error: Only serial ports are handled by this example.");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		@SuppressWarnings("rawtypes")
+		Enumeration ports2 = CommPortIdentifier.getPortIdentifiers();
+		LinkedList<String> ports = new LinkedList<String>();
+
+		int i = 0;
+		while (ports2.hasMoreElements()) {
+			i++;
+			CommPortIdentifier port = (CommPortIdentifier) ports2.nextElement();
+			String name = port.getName();
+
+			if (name.startsWith("/dev/ttyACM")
+					|| name.startsWith("/dev/ttyUSB")) {
+				System.out.println(i + ") " + name);
+				ports.push(port.getName());
+			}
+		}
+
+		Main m = new Main();
+		try {
+			for (String port : ports) {
+				System.out.println("Try " + port);
+				m.connect(port);
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		/*
+    */
+	}
+}
