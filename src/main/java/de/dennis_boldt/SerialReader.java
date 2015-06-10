bdbcf33af03548980ffcc7de11de52c2a606e3c7
Dennis Boldt <boldt@itm.uni-luebeck.de>
2015-06-10 10:22:45 +0200

    Code style

--- src/main/java/de/dennis_boldt/SerialReader.java
+++ src/main/java/de/dennis_boldt/SerialReader.java
@@ -4,22 +4,22 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class SerialReader implements Runnable {
- 
-    InputStream in;
- 
-    public SerialReader( InputStream in ) {
-      this.in = in;
-    }
- 
-    public void run() {
-      byte[] buffer = new byte[ 1024 ];
-      int len = -1;
-      try {
-        while( ( len = this.in.read( buffer ) ) > -1 ) {
-          System.out.print( new String( buffer, 0, len ) );
-        }
-      } catch( IOException e ) {
-        e.printStackTrace();
-      }
-    }
-  }
+
+	InputStream in;
+
+	public SerialReader(InputStream in) {
+		this.in = in;
+	}
+
+	public void run() {
+		byte[] buffer = new byte[1024];
+		int len = -1;
+		try {
+			while ((len = this.in.read(buffer)) > -1) {
+				System.out.print(new String(buffer, 0, len));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
